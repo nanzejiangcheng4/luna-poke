46ad7396189e8cb1c33601a8cbfaaedfbf9ff778
nanzejiangcheng4 <nanzejiangcheng4@gmail.com>
2026-05-08 14:37:08 +0900

    chore: setup Nuxt 4 project with Tailwind CSS

---
+++ app/pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { quotes } from "~/data/quotes";
+
+const randomQuote = ref("");
+
+onMounted(() => {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  randomQuote.value = quotes[randomIndex] ?? "なのら〜！";
+});
+</script>
+
+<template>
+  <div>
+    {{ randomQuote }}
+  </div>
+</template>
