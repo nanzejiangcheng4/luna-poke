99b85ff13d7d183ccfd9f5df0ce122a69e4899bc
nanzejiangcheng4 <nanzejiangcheng4@gmail.com>
2026-05-12 21:57:30 +0900

    インデックスページに Tailwind CSS のスタイルを追加しました。

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,14 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
   pages: true,
+  tailwindcss: {
+    config: {
+      content: [
+        "./app/components/**/*.vue",
+        "./app/layouts/**/*.vue",
+        "./app/pages/**/*.vue",
+        "./app/app.vue",
+      ],
+    },
+  },
 });
